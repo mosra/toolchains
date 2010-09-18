2c4678586622e40e2096a72766ddc9278e501ff1
Vladimír Vondruš <mosra@centrum.cz>
2010-09-18 21:57:08 +0200

    Translated comments.

--- Toolchain-Qt4-mingw32.cmake
+++ Toolchain-Qt4-mingw32.cmake
@@ -1,23 +1,23 @@
-# Jméno cílového systému
+# Target system name
 SET(CMAKE_SYSTEM_NAME   Windows)
 
-# Crosskompilátory
+# Compilers and utilities
 SET(CMAKE_C_COMPILER    i486-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER  i486-mingw32-g++)
 SET(CMAKE_STRIP         i486-mingw32-strip)
 SET(CMAKE_RC_COMPILER   i486-mingw32-windres)
 
-# Root pro crosskompilátor
+# Croscompiler path
 SET(CMAKE_FIND_ROOT_PATH /usr/i486-mingw32)
 
-# Programy jsou v prostředí systému, knihovny a include má u sebe
-# crosskompilátor
+# Find executables in root path, libraries and includes are in crosscompiler
+# path
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# FindQt4.cmake se ptá nativního cmake na proměnné, což zákonitě nemůže fungovat
-# při kroskompilaci.
+# FindQt4.cmake asks native QMake for variables, which surely cannot work when
+# crosscompiling.
 set(MINGW_PREFIX        /usr/i486-mingw32)
 set(QT_BINARY_DIR       ${MINGW_PREFIX}/bin)
 set(QT_LIBRARY_DIR      ${MINGW_PREFIX}/lib)
@@ -28,7 +28,7 @@ set(QT_QMAKE_EXECUTABLE /usr/bin/qmake)
 set(QT_UIC_EXECUTABLE   /usr/bin/uic)
 set(QT_RCC_EXECUTABLE   /usr/bin/rcc)
 
-# Moduly
+# Modules
 set(QT_QTCORE_INCLUDE_DIR       ${MINGW_PREFIX}/include/QtCore)
 set(QT_QTGUI_INCLUDE_DIR        ${MINGW_PREFIX}/include/QtGui)
 set(QT_QTNETWORK_INCLUDE_DIR    ${MINGW_PREFIX}/include/QtNetwork)
