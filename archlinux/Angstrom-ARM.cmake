208bc8bee5d99b8008e96cb15452b68596d2e608
Vladimír Vondruš <mosra@centrum.cz>
2012-07-12 13:40:32 +0200

    Added Angstrom-ARM.cmake for compiling with Angstrom toolchain.

---
+++ archlinux/Angstrom-ARM.cmake
@@ -0,0 +1,15 @@
+# Target system name
+SET(CMAKE_SYSTEM_NAME   Linux)
+
+# Compilers and utilities
+SET(CMAKE_C_COMPILER    arm-angstrom-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER  arm-angstrom-linux-gnueabi-g++)
+
+# Croscompiler paths
+SET(CMAKE_FIND_ROOT_PATH
+    /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr
+)
+
+# Libraries and includes are in crosscompiler path
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
