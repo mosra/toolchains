bbde1301797c86a87a93617a094d88ab4a0dda0a
Vladimír Vondruš <mosra@centrum.cz>
2012-11-10 20:03:55 +0100

    NaCl: base platform file on Linux.
    So it's possible to remove some stuff.

--- modules/Platform/NaCl.cmake
+++ modules/Platform/NaCl.cmake
@@ -2,18 +2,17 @@
 # Platform file for generic/NaCl-*.cmake toolchains
 #
 
-set(CMAKE_C_OUTPUT_EXTENSION ".o")
-set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+# Start from something sane and working
+include(Platform/Linux)
+
 set(CMAKE_EXECUTABLE_SUFFIX "-${NACL_ARCH_NMF}.nexe")
 
+# TODO: This is getting overrriden somehow
 set(CMAKE_C_FLAGS "${NACL_ARCH_GCC}")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_COMPILER_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
 
 # Otherwise .so have paths instead of library names
 set(CMAKE_PLATFORM_USES_PATH_WHEN_NO_SONAME 1)
 
 # TODO: Will this need -lpthread also?
 set(CMAKE_EXE_LINKER_FLAGS "-lppapi_cpp -lppapi")
-
-# So we have proper paths for finding stuff
-include(Platform/UnixPaths)
