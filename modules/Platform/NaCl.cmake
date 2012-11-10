efda9628e61f7489ed3eeb3fd0d0b28f0bf7fd1e
Vladimír Vondruš <mosra@centrum.cz>
2012-11-10 04:51:41 +0100

    NaCl: don't use absolute library paths when linking.

--- modules/Platform/NaCl.cmake
+++ modules/Platform/NaCl.cmake
@@ -9,5 +9,8 @@ set(CMAKE_EXECUTABLE_SUFFIX "-${NACL_ARCH_NMF}.nexe")
 set(CMAKE_C_FLAGS "${NACL_ARCH_GCC}")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_COMPILER_FLAGS}")
 
+# Otherwise .so have paths instead of library names
+set(CMAKE_PLATFORM_USES_PATH_WHEN_NO_SONAME 1)
+
 # TODO: Will this need -lpthread also?
 set(CMAKE_EXE_LINKER_FLAGS "-lppapi_cpp -lppapi")
