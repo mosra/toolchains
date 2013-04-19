8d9a7bfe66617b58b21d7374d4625adf75b98651
Vladimír Vondruš <mosra@centrum.cz>
2013-04-19 23:37:04 +0200

    NaCl: removed obsolete cruft.
    This thing is handled already in Platform/Linux.cmake.

--- modules/Platform/NaCl.cmake
+++ modules/Platform/NaCl.cmake
@@ -10,8 +10,3 @@ set(CMAKE_EXECUTABLE_SUFFIX "-${NACL_ARCH_NMF}.nexe")
 # TODO: This is getting overrriden somehow
 set(CMAKE_C_FLAGS "${NACL_ARCH_GCC}")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
-
-# Otherwise .so have paths instead of library names
-set(CMAKE_PLATFORM_USES_PATH_WHEN_NO_SONAME 1)
-
-# TODO: Will this need -lpthread also?
