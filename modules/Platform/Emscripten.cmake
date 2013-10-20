9599b8d5c69432415249c085e68612784fc6fae9
Vladimír Vondruš <mosra@centrum.cz>
2013-10-20 14:50:49 +0200

    Emscripten: use Linux-specific prefix/suffix for shared libraries.
    Otherwise CMake is unable to find the libraries. Will look into that
    later.

--- modules/Platform/Emscripten.cmake
+++ modules/Platform/Emscripten.cmake
@@ -5,6 +5,7 @@
 # Start from something sane and working
 include(Platform/Linux)
 
-set(CMAKE_STATIC_LIBRARY_PREFIX "")
-set(CMAKE_STATIC_LIBRARY_SUFFIX ".bc")
+# TODO: with this suffix CMake is unable to find the libraries
+# set(CMAKE_STATIC_LIBRARY_PREFIX "")
+# set(CMAKE_STATIC_LIBRARY_SUFFIX ".bc")
 set(CMAKE_EXECUTABLE_SUFFIX ".js")
