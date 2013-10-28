8e6dbeeb661ad0bd2d678b9f6bfb9e9044eb45f9
Vladimír Vondruš <mosra@centrum.cz>
2013-10-28 18:30:38 +0100

    Emscripten: apparently I need to set CACHE STRING for them to take effect.
    Why how what.

--- modules/Platform/Emscripten.cmake
+++ modules/Platform/Emscripten.cmake
@@ -15,5 +15,5 @@ set(CMAKE_FIND_LIBRARY_PREFIXES "")
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".bc")
 
 # Best possible options for code size in release mode
-set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O2 --closure 1")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O2 --closure 1 --llvm-lto 3")
+set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O2 --closure 1" CACHE STRING "Flags used by Emscripten compiler for Release builds")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O2 --closure 1 --llvm-lto 3" CACHE STRING "Flags used by Emscripten linker for Release builds")
