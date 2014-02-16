a1e183f28d5f4a20b9bf83352105bf12b1e9444d
Vladimír Vondruš <mosra@centrum.cz>
2014-02-16 20:36:36 +0100

    Initial version of Android NDK x86/ARM toolchain.

---
+++ generic/Android-ARM.cmake
@@ -0,0 +1,49 @@
+#
+# Toolchain for cross-compiling to Android ARM.
+#
+# Modify ANDROID_NDK_ROOT and ANDROID_API_LEVEL to your liking. You might also
+# need to update ANDROID_TOOLCHAIN_PREFIX and ANDROID_TOOLCHAIN_ROOT to fit
+# your system. You have to add modules/ directory to CMAKE_MODULE_PATH before
+# using the toolchain file so Android platform file can be found, e.g.:
+#
+#  mkdir build-android-arm && cd build-android-arm
+#  cmake .. \
+#       -DCMAKE_MODULE_PATH=/absolute/path/to/toolchains/modules \
+#       -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake
+#
+# Shared library compiled using this toolchain should behave the same as the
+# one compiled with ndk-build. The libraries are then moved into
+# libs/${ANDROID_ABI} to make it available for ant.
+#
+
+set(CMAKE_SYSTEM_NAME Android)
+
+# NDK root
+set(ANDROID_NDK_ROOT "/opt/android-ndk")
+
+# API level to use
+set(ANDROID_API_LEVEL "android-19")
+
+# Toolchain. See ${ANDROID_NDK_ROOT}/toolchains/ for complete list
+set(ANDROID_ARCHITECTURE "arm")
+set(ANDROID_ABI "armeabi-v7a")
+set(ANDROID_TOOLCHAIN "arm-linux-androideabi-4.8")
+set(ANDROID_TOOLCHAIN_PREFIX "arm-linux-androideabi")
+set(ANDROID_TOOLCHAIN_ROOT "${ANDROID_NDK_ROOT}/toolchains/${ANDROID_TOOLCHAIN}/prebuilt/linux-x86_64/")
+
+set(CMAKE_C_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-gcc")
+set(CMAKE_CXX_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-g++")
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}
+    "${ANDROID_NDK_ROOT}/platform/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}"
+    "${ANDROID_TOOLCHAIN_ROOT}")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Native App Glue
+set(ANDROID_NATIVE_APP_GLUE_INCLUDE_DIR "${ANDROID_NDK_ROOT}/sources/android/native_app_glue/")
+set(ANDROID_NATIVE_APP_GLUE_SRC "${ANDROID_NATIVE_APP_GLUE_INCLUDE_DIR}/android_native_app_glue.c")
+
+# Output dir for compiled stuff
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/libs/${ANDROID_ABI}")
---
+++ modules/Platform/Android.cmake
@@ -0,0 +1,62 @@
+#
+# Platform file for generic/Android.cmake toolchain
+#
+
+# Start from something sane and working
+include(Platform/Linux)
+
+# Reset the values so they don't accumulate
+set(CMAKE_C_FLAGS )
+set(CMAKE_CXX_FLAGS )
+set(CMAKE_EXE_LINKER_FLAGS )
+
+# ARM-specific flags
+if(${ANDROID_ARCHITECTURE} STREQUAL "arm")
+
+    # Why do we need this?
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+
+    # Have consistent FP ABI, use Neon by default
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=softfp -mfpu=neon")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfloat-abi=softfp -mfpu=neon")
+
+    # Fix hardware error in some Cortex A8 implementations
+    # (is this still needed or already enabled by default?)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--fix-cortex-a8")
+
+    if(${ANDROID_ABI} STREQUAL "armeabi-v7a")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=armv7-a")
+    else()
+        message(FATAL_ERROR "Unsupported Android ARM ABI ${ANDROID_ABI}")
+    endif()
+
+# x86-specific flags
+elseif(${ANDROID_ARCHITECTURE} STREQUAL "x86")
+    # (None)
+
+# Otherwise unsupported
+else()
+    message(FATAL_ERROR "Unsupported Android ABI ${ANDROID_ABI}")
+endif()
+
+# Fix distributed build on GCC 4.6+
+# https://android-review.googlesource.com/#/c/47564/
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -no-canonical-prefixes")
+
+# Disallow undefined symbols, memory corruption mitigation, etc.
+# This is needed to ensure that the native_app_glue is really linked in
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wa,--noexecstack")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wa,--noexecstack")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now")
+
+# Specify sysroot so the compiler and linker can find stuff
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --sysroot=${ANDROID_NDK_ROOT}/platforms/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --sysroot=${ANDROID_NDK_ROOT}/platforms/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${ANDROID_NDK_ROOT}/platforms/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}")
+
+# Make sure the flags don't get overriden
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C compiler flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "CXX compiler flags")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "Linker flags")
