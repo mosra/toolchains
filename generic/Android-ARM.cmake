4375ed118ae854182cfa4fdb6c533356fea8dea8
Vladimír Vondruš <mosra@centrum.cz>
2014-02-20 12:38:34 +0100

    Android: get rid of superfluous variables.

--- generic/Android-ARM.cmake
+++ generic/Android-ARM.cmake
@@ -1,7 +1,7 @@
 #
 # Toolchain for cross-compiling to Android ARM.
 #
-# Modify ANDROID_NDK_ROOT and ANDROID_API_LEVEL to your liking. You might also
+# Modify ANDROID_NDK_ROOT and ANDROID_SYSROOT to your liking. You might also
 # need to update ANDROID_TOOLCHAIN_PREFIX and ANDROID_TOOLCHAIN_ROOT to fit
 # your system. You have to add modules/ directory to CMAKE_MODULE_PATH before
 # using the toolchain file so Android platform file can be found, e.g.:
@@ -19,20 +19,19 @@
 #
 
 set(CMAKE_SYSTEM_NAME Android)
+set(ANDROID_ARCHITECTURE "arm")
+set(ANDROID_ABI "armeabi-v7a")
 
 # NDK root
 set(ANDROID_NDK_ROOT "/opt/android-ndk")
 
 # API level to use
-set(ANDROID_API_LEVEL "android-19")
+set(ANDROID_SYSROOT "${ANDROID_NDK_ROOT}/platforms/android-19/arch-${ANDROID_ARCHITECTURE}")
 
 # Toolchain. See ${ANDROID_NDK_ROOT}/toolchains/ for complete list
-set(ANDROID_ARCHITECTURE "arm")
-set(ANDROID_ABI "armeabi-v7a")
 set(ANDROID_TOOLCHAIN "arm-linux-androideabi-4.8")
 set(ANDROID_TOOLCHAIN_PREFIX "arm-linux-androideabi")
 set(ANDROID_TOOLCHAIN_ROOT "${ANDROID_NDK_ROOT}/toolchains/${ANDROID_TOOLCHAIN}/prebuilt/linux-x86_64/")
-set(ANDROID_SYSROOT "${ANDROID_NDK_ROOT}/platforms/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}")
 
 set(CMAKE_C_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-gcc")
 set(CMAKE_CXX_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-g++")
--- generic/Android-x86.cmake
+++ generic/Android-x86.cmake
@@ -1,7 +1,7 @@
 #
 # Toolchain for cross-compiling to Android x86.
 #
-# Modify ANDROID_NDK_ROOT and ANDROID_API_LEVEL to your liking. You might also
+# Modify ANDROID_NDK_ROOT and ANDROID_SYSROOT to your liking. You might also
 # need to update ANDROID_TOOLCHAIN_PREFIX and ANDROID_TOOLCHAIN_ROOT to fit
 # your system. You have to add modules/ directory to CMAKE_MODULE_PATH before
 # using the toolchain file so Android platform file can be found, e.g.:
@@ -19,20 +19,19 @@
 #
 
 set(CMAKE_SYSTEM_NAME Android)
+set(ANDROID_ARCHITECTURE "x86")
+set(ANDROID_ABI "x86")
 
 # NDK root
 set(ANDROID_NDK_ROOT "/opt/android-ndk")
 
 # API level to use
-set(ANDROID_API_LEVEL "android-19")
+set(ANDROID_SYSROOT "${ANDROID_NDK_ROOT}/platforms/android-19/arch-${ANDROID_ARCHITECTURE}")
 
 # Toolchain. See ${ANDROID_NDK_ROOT}/toolchains/ for complete list
-set(ANDROID_ARCHITECTURE "x86")
-set(ANDROID_ABI "x86")
 set(ANDROID_TOOLCHAIN "x86-4.8")
 set(ANDROID_TOOLCHAIN_PREFIX "i686-linux-android")
 set(ANDROID_TOOLCHAIN_ROOT "${ANDROID_NDK_ROOT}/toolchains/${ANDROID_TOOLCHAIN}/prebuilt/linux-x86_64/")
-set(ANDROID_SYSROOT "${ANDROID_NDK_ROOT}/platforms/${ANDROID_API_LEVEL}/arch-${ANDROID_ARCHITECTURE}")
 
 set(CMAKE_C_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-gcc")
 set(CMAKE_CXX_COMPILER "${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_TOOLCHAIN_PREFIX}-g++")
