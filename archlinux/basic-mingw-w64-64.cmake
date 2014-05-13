19d1e7ee8bc61a6e1b4871c93e3aca4743e0ca9e
Vladimír Vondruš <mosra@centrum.cz>
2014-05-13 20:29:59 +0200

    Specify CMAKE_RC_COMPILER also for MinGW-w64.

--- archlinux/basic-mingw-w64-64.cmake
+++ archlinux/basic-mingw-w64-64.cmake
@@ -12,6 +12,7 @@ set(CMAKE_SYSTEM_NAME   Windows)
 # Compilers and utilities
 set(CMAKE_C_COMPILER    x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER  x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER   x86_64-w64-mingw32-windres)
 
 # Croscompiler path
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}
