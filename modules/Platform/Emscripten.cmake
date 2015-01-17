9108c58bf70bcd39be7be886d9aa837f27743646
Vladimír Vondruš <mosra@centrum.cz>
2015-01-17 15:53:38 +0100

    Emscripten: disable annoying warning about absolute include paths.

--- modules/Platform/Emscripten.cmake
+++ modules/Platform/Emscripten.cmake
@@ -14,6 +14,9 @@ set(CMAKE_EXECUTABLE_SUFFIX ".js")
 set(CMAKE_FIND_LIBRARY_PREFIXES "")
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".bc")
 
+# Disable annoying warning about absolute includes
+set(CMAKE_CXX_FLAGS "-Wno-warn-absolute-paths")
+
 # Best possible options for code size in release mode
 set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O2 --closure 1" CACHE STRING "Flags used by Emscripten compiler for Release builds")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O2 --closure 1 --llvm-lto 2" CACHE STRING "Flags used by Emscripten linker for Release builds")
