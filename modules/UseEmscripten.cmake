3de9f690ff38c733aebaf86af75fd7bf680fb130
Vladimír Vondruš <mosra@centrum.cz>
2015-12-06 23:14:14 +0100

    modules: added UseEmscripten.cmake.

---
+++ modules/UseEmscripten.cmake
@@ -0,0 +1,17 @@
+# - Macros for simplifying development with Emscripten
+#
+# Embed file into the virtual filesystem of given executable
+#  emscripten_embed_file(MyExecutable
+#                   path/to/file
+#                   /path/in/virtual/filesystem)
+# File path is relative to current source directory, path in virtual filesystem
+# should start with `/`. Supports both files and directories. Useful e.g. in
+# case of unit tests where you are testing output against some external file
+# and manually embedding that file just to test on Emscripten is just not worth
+# it.
+#
+
+function(emscripten_embed_file target file destination)
+    get_target_property(${target}_LINK_FLAGS ${target} LINK_FLAGS)
+    set_target_properties(${target} PROPERTIES LINK_FLAGS "${${target}_LINK_FLAGS} --embed-file ${CMAKE_CURRENT_SOURCE_DIR}/${file}@${destination}")
+endfunction()
