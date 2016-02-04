4948271ce74f7db798cb1282fbb7f55b90dc8c57
Vladimír Vondruš <mosra@centrum.cz>
2016-02-04 18:28:58 +0100

    Added iOS toolchain.

---
+++ generic/iOS.cmake
@@ -0,0 +1,20 @@
+#
+# Toolchain for cross-compiling to iOS using Xcode
+#
+# Set CMAKE_OSX_ROOT to SDK you want to target and enable all desired
+# architectures in CMAKE_OSX_ARCHITECTURES. You have to addd modules/ directory
+# to CMAKE_MODULE_PATH before using the toolchain file so the iOS platform file
+# can be found.
+#
+#  mkdir build-ios && cd build-ios
+#  cmake -DCMAKE_MODULE_PATH=/path/to/toolchains/modules/ \
+#        -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/iOS.cmake \
+#        -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
+#        -DCMAKE_OSX_ARCHITECTURES="arm64;armv7;armv7s" -G Xcode ..
+#
+
+# We just need to name the platform differently so CMAKE_CROSSCOMPILING is set
+set(CMAKE_SYSTEM_NAME "iOS")
+
+# Required to make CMake's test_compile stuff pass
+set(CMAKE_MACOSX_BUNDLE ON)
---
+++ modules/Platform/iOS.cmake
@@ -0,0 +1,6 @@
+#
+# Platform file for generic/iOS.cmake toolchain
+#
+
+# It's just Darwin after all
+include(Platform/Darwin)
