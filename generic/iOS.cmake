a5f2bf4387c293e0cc81248af0b5c673c3c5f61e
Vladimír Vondruš <mosra@centrum.cz>
2016-03-17 22:40:50 +0100

    Doc++

--- generic/iOS.cmake
+++ generic/iOS.cmake
@@ -2,15 +2,16 @@
 # Toolchain for cross-compiling to iOS using Xcode
 #
 # Set CMAKE_OSX_ROOT to SDK you want to target and enable all desired
-# architectures in CMAKE_OSX_ARCHITECTURES. You have to addd modules/ directory
-# to CMAKE_MODULE_PATH before using the toolchain file so the iOS platform file
-# can be found.
+# architectures in CMAKE_OSX_ARCHITECTURES. The toolchain needs access to its
+# platform file, so be sure to properly set absolute path to modules/ directory
+# containing Platform/iOS.cmake.
 #
 #  mkdir build-ios && cd build-ios
-#  cmake -DCMAKE_MODULE_PATH=/path/to/toolchains/modules/ \
-#        -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/iOS.cmake \
-#        -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
-#        -DCMAKE_OSX_ARCHITECTURES="arm64;armv7;armv7s" -G Xcode ..
+#  cmake .. \
+#       -DCMAKE_MODULE_PATH=/absolute/path/to/toolchains/modules/ \
+#       -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/iOS.cmake \
+#       -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
+#       -DCMAKE_OSX_ARCHITECTURES="arm64;armv7;armv7s" -G Xcode
 #
 
 # We just need to name the platform differently so CMAKE_CROSSCOMPILING is set
