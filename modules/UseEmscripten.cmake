1e2799dc30540b5d0a90395d654aec63116e9d6e
Vladimír Vondruš <mosra@centrum.cz>
2016-11-26 19:38:48 +0100

    UseEmscripten: properly handle absolute paths in emscripten_embed_file().

--- modules/UseEmscripten.cmake
+++ modules/UseEmscripten.cmake
@@ -4,14 +4,15 @@
 #  emscripten_embed_file(MyExecutable
 #                   path/to/file
 #                   /path/in/virtual/filesystem)
-# File path is relative to current source directory, path in virtual filesystem
-# should start with `/`. Supports both files and directories. Useful e.g. in
-# case of unit tests where you are testing output against some external file
-# and manually embedding that file just to test on Emscripten is just not worth
-# it.
+# File path is absolute or relative to current source directory, path in
+# virtual filesystem should start with `/`. Supports both files and
+# directories. Useful e.g. in case of unit tests where you are testing output
+# against some external file and manually embedding that file just to test on
+# Emscripten is just not worth it.
 #
 
 function(emscripten_embed_file target file destination)
+    get_filename_component(absolute_file ${file} ABSOLUTE)
     get_target_property(${target}_LINK_FLAGS ${target} LINK_FLAGS)
-    set_target_properties(${target} PROPERTIES LINK_FLAGS "${${target}_LINK_FLAGS} --embed-file ${CMAKE_CURRENT_SOURCE_DIR}/${file}@${destination}")
+    set_target_properties(${target} PROPERTIES LINK_FLAGS "${${target}_LINK_FLAGS} --embed-file ${absolute_file}@${destination}")
 endfunction()
