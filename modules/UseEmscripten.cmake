fbee8340f5c5a5bb47b784f61b19549dfa24c31d
Vladimír Vondruš <mosra@centrum.cz>
2017-09-05 21:50:39 +0200

    Fix emscripten_embed_file() in case target's LINK_FLAGS are not set.
    What a stupid behavior of get_target_property().

--- modules/UseEmscripten.cmake
+++ modules/UseEmscripten.cmake
@@ -14,5 +14,8 @@
 function(emscripten_embed_file target file destination)
     get_filename_component(absolute_file ${file} ABSOLUTE)
     get_target_property(${target}_LINK_FLAGS ${target} LINK_FLAGS)
+    if(NOT ${target}_LINK_FLAGS)
+        set(${target}_LINK_FLAGS )
+    endif()
     set_target_properties(${target} PROPERTIES LINK_FLAGS "${${target}_LINK_FLAGS} --embed-file ${absolute_file}@${destination}")
 endfunction()
