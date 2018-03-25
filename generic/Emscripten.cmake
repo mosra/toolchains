579f8dfed36afd63e4919f89dcc0bacb8286a8af
Vladimír Vondruš <mosra@centrum.cz>
2018-03-25 19:04:20 +0200

    Emscripten: drop the closure compiler.
    It's only pain and misery.

--- generic/Emscripten.cmake
+++ generic/Emscripten.cmake
@@ -44,5 +44,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 # Best possible options for code size in release mode. The *_INIT variables are
 # available since CMake 3.7, so it won't work in earlier versions. Sorry.
 cmake_minimum_required(VERSION 3.7)
-set(CMAKE_CXX_FLAGS_RELEASE_INIT "-DNDEBUG -O3 --closure 1")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE_INIT "-O3 --closure 1 --llvm-lto 1")
+set(CMAKE_CXX_FLAGS_RELEASE_INIT " -DNDEBUG -O3")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE_INIT " -O3 --llvm-lto 1")
