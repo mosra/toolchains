6a9e0821b4ccd578feee1f30cef245cbd36cdd38
Vladimír Vondruš <mosra@centrum.cz>
2023-09-04 22:41:40 +0200

    Emscripten: add the *non-cache* include dir to implicit include dirs.

--- modules/Platform/Emscripten.cmake
+++ modules/Platform/Emscripten.cmake
@@ -41,5 +41,27 @@ set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
 # amount of spam messages when including third-party headers.
 set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem")
 
+# With Emscripten copying all its includes (but not libraries!) into the cache
+# on 2.0.13, CMake configs (such as zstd's) have to be put into share/ (instead
+# of lib/cmake/, which also gets copied) in the system directory in order to be
+# able to locate the libraries. That however means the imported targets from
+# those configs will use the system include directory, and passing it to the
+# include path would result in issues with <emscripten/version.h> again. To fix
+# that, this directory is marked as implicit. Which it actually isn't, but that
+# doesn't matter, since the exact contents should be in its copy in the cache,
+# which *is* among implicit include directories.
+#
+# This is slightly (but not completely) related to
+#   https://github.com/emscripten-core/emscripten/issues/17132
+# with the difference that there it caused an ordering issue with the
+# `include/common`, while here is no ordering issue but it causes the wrong
+# <emscripten/version.h> to be picked. In other words, again NONE of this crap
+# would be needed if the doomed-to-fail version.h wasn't in the system include
+# dir at all.
+if(NOT EMSCRIPTEN_VERSION VERSION_LESS 2.0.13)
+    list(APPEND CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES ${EMSCRIPTEN_PREFIX}/system/include)
+    list(APPEND CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES ${EMSCRIPTEN_PREFIX}/system/include)
+endif()
+
 # Include the UseEmscripten file with useful macros
 include(UseEmscripten)
